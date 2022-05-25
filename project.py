70d281e286e6f8429e1c712930091914ad99da42
Partha Sarathi Hazra <sarathiparthahazra9609@gmail.com>
2022-05-25 12:03:09 +0530

    initial commit

---
+++ project.py
@@ -0,0 +1,48 @@
+print("Welcome To My Quiz")
+
+playing = input("Do You Want To Play? ")
+
+if playing.lower() != "yes":
+    quit()
+
+print("Okey! Let's Play :)")
+score = 0
+
+answer = input("What Does CPU Stand For? ")
+if answer.lower() =="central processing unit":
+    print("Correct!")
+    score+=1
+else:
+    print("Incorrect!")
+    
+answer = input("What Is GPU Stands For? ")
+if answer.lower() =="graphics processing unit":
+    print("Correct!")
+    score+=1
+else:
+    print("Incorrect!")
+
+answer = input("What Is RAM Stands For? ")
+if answer.lower() =="random access memory":
+    print("Correct!")
+    score+=1
+else:
+    print("Incorrect!")
+    
+answer = input("What Is ROM Stands For? ")
+if answer.lower() =="read only memory":
+    print("Correct!")
+    score+=1
+else:
+    print("Incorrect!")
+
+
+answer = input("Who Is The Prime Minister Of India? ")
+if answer.lower() =="narendra modi":
+    print("Correct!")
+    score+=1
+else:
+    print("Incorrect!")
+    
+print("You Give  " + str(score) +  "  Answer Correct!")
+print("You Got  " + str(score*100/5) +  "%.")
