ab08cfbe6810b352f6dfc0b4218326fdf01df843
Partha Sarathi Hazra <sarathiparthahazra9609@gmail.com>
2022-05-26 16:31:55 +0530

    i add a new project

---
+++ project5.py
@@ -0,0 +1,29 @@
+#password manager(Advance
+
+
+name = input("What Is Your Name ..")
+
+def add():
+    name = input('Account Name :')
+    pwd = input("Password: ")
+    
+    with open('passwords.txt','a') as f: #for creating a new text file
+        f.write(name + "|" + pwd + "\n") # for storing name and password
+        
+def view():
+    with open('passwords.txt','r') as f: 
+        for line in f.readlines():
+            data =line.rstrip()
+            print(line)
+while True:
+    mode = input("would you like to add a new password or view existing ones (view,add), press q to quit? ").lower()
+    if mode =="q":
+        break
+    
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+         print("Invalid Mode.")
+         continue
